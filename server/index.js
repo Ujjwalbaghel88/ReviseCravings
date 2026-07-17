1f932d8de3c6b697ec3e0fdc2f3816ebd52e9937
Ujjwalbaghel88 <ujjwalbaghel800@gmail.com>
2026-07-17 22:12:19 +0530

    Remove spaces

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+
 const app = express()
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
